6eedde4a4abc91837de45f72c3c7ceb13b9c0eb7
Ebarretorho <ebarretorho@gmail.com>
2022-04-12 22:11:21 -0400

    where we are right now

---
+++ New folder/app.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, inspect, func
+
+from flask import Flask, jsonify, render_template 
+
+#################################################
+# Database Setup
+#################################################
+SERVER = '127.0.0.1'
+DATABASE = 'project_3'
+USERNAME = 'postgres'
+PASSWORD_2 = 'postgres'
+DATABASE_CONN = f'postgresql://{USERNAME}:{PASSWORD_2}@{SERVER}/{DATABASE}'
+engine = create_engine(DATABASE_CONN)
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+#################################################
+# Flask Routes
+#################################################
+session = Session(engine)
+results = engine.execute('SELECT * FROM crops_climate_data').fetchall()
+session.close()
+
+
+# create route that renders index.html template
+@app.route("/")
+def index():
+
+    return render_template("index.html")
+
+@app.route("/data/api")
+def data_crops():
+    all_crops = []
+    for year, commodity , value, volume, price, temp, rain in results:
+        production_dict = {}
+        production_dict["year"] = year
+        production_dict["commodity"] = commodity
+        production_dict["value"] = f'{value}'
+        production_dict["volume"] = f'{volume}'
+        production_dict["avgprice"] = f'{price}'
+        production_dict["avgtemperature"] = f'{temp}'
+        production_dict["totalprecipitation"] = f'{rain}'
+        
+        all_crops.append(production_dict)
+
+
+    return jsonify(all_crops)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ New folder/static/js/plot1.js
@@ -0,0 +1,31 @@
+
+url_data = "http://127.0.0.1:5000/data/api";
+
+d3.json(url_data).then(data => {
+  // console.log(data);
+  corns = data.filter(row => row.commodity === 'CORN');
+  console.log(corns);
+
+  names = data.map(function (row) {
+    return row.year
+  });
+
+  // Trace for the Greek Data
+  let trace1 = {
+    x: data.map(row => row.year),
+    y: data.map(row => row.value),
+    type: "bar"
+  };
+
+
+  // Data trace array
+  let traceData = [trace1];
+
+  // Apply the group barmode to the layout
+  let layout = {
+    title: "Podruction $$ per Year per Crop"
+  };
+
+  // Render the plot to the div tag with id "plot"
+  Plotly.newPlot("plot", traceData, layout);
+});  
